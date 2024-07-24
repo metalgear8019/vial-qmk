4b9ae67e2adf21a4814d7136a80634bc2bc30b0a
metalgear8019 <metalgear8019@gmail.com>
2024-07-24 17:08:52 +1000

    Update wellum rules to match guide

--- keyboards/crkbd/keymaps/wellum/rules.mk
+++ keyboards/crkbd/keymaps/wellum/rules.mk
@@ -1,4 +1,18 @@
 RGBLIGHT_ENABLE = no
 
+NKRO_ENABLE = yes # Enable N-Key Rollover
+BOOTMAGIC_ENABLE = yes # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+LTO_ENABLE = no
+VIA_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+REPEAT_KEY_ENABLE = yes
+AUTO_SHIFT_ENABLE = yes
+
 SRC += ./oneshot.c
 SRC += ./swapper.c
